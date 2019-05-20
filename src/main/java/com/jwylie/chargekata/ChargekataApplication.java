83139bdd1042f064f95e6402dc9602eb7f38b000
Jack Wylie <jwylie@manifestcorp.com>
2019-05-20 09:17:28 -0400

    Created interfaces

---
+++ src/main/java/com/jwylie/chargekata/ChargekataApplication.java
@@ -0,0 +1,13 @@
+package com.jwylie.chargekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargekataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargekataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jwylie/chargekata/User.java
@@ -0,0 +1,11 @@
+package com.jwylie.chargekata;
+
+public interface User {
+
+	public String getId();
+	public String getUsername();
+	
+	public void setId(String id);
+	public void setUsername(String username);
+	
+}
